87c42241d7b49851450b3b5d9493941eef3c4feb
Mobin <Mobinmojiri1234@gmail.com>
2025-03-01 20:12:31 +0330

    Making PersonClass and its method

---
+++ src/Base/PersonClass.java
@@ -0,0 +1,34 @@
+package Base;
+import java.util.ArrayList;
+
+public class PersonClass {
+    public static int personId = 1001;
+    public static ArrayList<PersonClass> personList = new ArrayList<>();
+
+
+    public int personID;
+    public int nationalId;
+    public String name;
+
+    public PersonClass(String name, int nationalId){
+        this.personID = personId;
+        personId++;
+        this.name = name;
+        personList.add(this);
+        this.nationalId = nationalId;
+    }
+    public static PersonClass findBYid(int ID){
+        for(PersonClass person: personList){
+            if(person.personID == ID){
+                System.out.println("YES");
+                return person;
+            }
+        }
+        System.out.println("Student does not exist!");
+        return null;
+    }
+
+}
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import Base.PersonClass;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
